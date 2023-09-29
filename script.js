6d1e97036be734b316cd104ba0905e3976f8c88f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 11:05:57 -0300

    commitFinal

---
+++ script.js
@@ -0,0 +1,27 @@
+let nome = "Felipe"
+
+let idade = 25
+
+let status = true
+
+let valorProduto = 10.50
+
+let listaTimes = ["Palmeiras", "São Paulo", "Flamengo"]
+
+console.log(nome)
+console.log(idade)
+console.log(status)
+console.log(valorProduto)
+console.log(listaTimes)
+
+let soma = 5 + 7
+console.log(soma)
+
+let subtracao = 9 - 7
+console.log(subtracao)
+
+let multiplicacao = 5 * 5
+console.log(multiplicacao)
+
+let divisao = 21 / 7
+console.log(divisao)
